5c674515fc7ed2977a17e07d2a21c2c7b83393d4
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-02-27 21:20:01 +0100

    Add validateIp helper function

--- include/helpers.php
+++ include/helpers.php
@@ -1,3 +1,17 @@
 <?php
 
+function validateIp($ip) {
+    $pieces = explode(".", $ip);
+    if (count($pieces) != 4)
+        return false;
+    foreach ($pieces as $piece) {
+        $intpiece = (int)$piece;
+        if ((string)$intpiece != $piece)
+            return false;
+        if (($intpiece < 0) || ($intpiece > 255))
+            return false;
+    }
+    return true;
+}
+
 ?>
