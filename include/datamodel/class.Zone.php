2bf5a2bcb70a78e83341bb376f0b71103c871adc
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-02-27 22:39:03 +0100

    Implement Zone updating

--- include/datamodel/class.Zone.php
+++ include/datamodel/class.Zone.php
@@ -135,6 +135,46 @@ class Zone extends DataObject {
         return false;
     }
 
+    public function generateFile() {
+
+        list($rname1, $rname2) = explode('@', $this->data[$this->fieldName(self::PROPERTY_SOA_RNAME)]);
+        $rname1 = str_replace('.', '\.', $rname1);
+        $rname = $rname1 . '.' . $rname2;
+
+        $soa_serial_date = &$this->data[$this->fieldName(self::PROPERTY_SOA_SERIAL_DATE)];
+        $soa_serial_counter = &$this->data[$this->fieldName(self::PROPERTY_SOA_SERIAL_COUNTER)];
+        $today = date('Y-m-d');
+        if ($soa_serial_date != $today) {
+            $soa_serial_date = $today;
+            $soa_serial_counter = 1;
+        } else {
+            $soa_serial_counter++;
+        }
+        $soa_serial = str_replace('-', '', $soa_serial_date);
+        $soa_serial .= ($soa_serial_counter < 100) ? sprintf('%02d', $soa_serial) : $soa_serial;
+
+        $fileContent = sprintf('$ORIGIN %s\n', $this->data[$this->fieldName(self::PROPERTY_ORIGIN)]);
+        $fileContent .= sprintf('$TTL %d\n', $this->data[$this->fieldName(self::PROPERTY_TTL)]);
+        $fileContent .= sprintf('@ IN SOA %s %s (\n', $this->data[$this->fieldName(self::PROPERTY_SOA_MNAME)], $rname);
+        $fileContent .= sprintf('  %d ; Serial\n', $soa_serial);
+        $fileContent .= sprintf('  %d ; Refresh\n', $this->data[$this->fieldName(self::PROPERTY_SOA_REFRESH)]);
+        $fileContent .= sprintf('  %d ; Retry\n', $this->data[$this->fieldName(self::PROPERTY_SOA_RETRY)]);
+        $fileContent .= sprintf('  %d ; Expire\n', $this->data[$this->fieldName(self::PROPERTY_SOA_EXPIRE)]);
+        $fileContent .= sprintf('  %d ; Negative TTL\n', $this->data[$this->fieldName(self::PROPERTY_SOA_TTL)]);
+        $fileContent .= ')\n';
+
+        foreach ($this->getExtraRecords() as $extraRecord)
+            $fileContent .= sprintf('%s IN %s %s\n', $extraRecord->name, $extraRecord->type, $extraRecord->value);
+
+        foreach ($this->getDomains() as $domain)
+            $fileContent .= sprintf('%s IN A %s\n', $domain->name, $domain->ip_address);
+
+        file_put_contents($this->data[$this->fieldName(self::PROPERTY_FILE)], $fileContent);
+        $this->data[$this->fieldName(self::PROPERTY_FILE_LAST_UPDATE)] = time();
+        $this->save();
+
+    }
+
 }
 
 ?>
