8dcd98e5bf3637c353e425a4d846424e82f35774
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-02-27 21:20:39 +0100

    Implement Domain::update function

--- include/datamodel/class.Domain.php
+++ include/datamodel/class.Domain.php
@@ -84,6 +84,33 @@ class Domain extends DataObject {
         return new Zone($this->data[$this->fieldName(self::PROPERTY_ZONE_ID)]);
     }
 
+    public function update($key1, $hash2, $ip) {
+        if (!validateIp($ip))
+            return false;
+        if ($key1 != $this->data[$this->fieldName(self::PROPERTY_UPDATE_KEY_1)])
+            return false;
+        if (!$this->checkHash2($hash2))
+            return false;
+        $this->data[$this->fieldName(self::PROPERTY_IP_ADDRESS)] = $ip;
+        $this->data[$this->fieldName(self::PROPERTY_LAST_UPDATE)] = time();
+        $this->save();
+        return true;
+    }
+
+    public function checkHash2($hash2) {
+        $startTime = time();
+        for ($i = KEY2_VALID_TIME_PRE; $i >= KEY2_VALID_TIME_POST; $i++) {
+            $t = $startTime + $i;
+            $h = md5(sprintf("%s:%s",
+                $this->data[$this->fieldName(self::PROPERTY_UPDATE_KEY_2)],
+                date("YmdHis", $t)
+            ));
+            if ($hash2 == $h)
+                return true;
+        }
+        return false;
+    }
+
 }
 
 ?>
