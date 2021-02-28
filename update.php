eb52028818a2a83f6767e808b15efe2560a62b01
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-02-28 10:28:28 +0100

    Add update.php

---
+++ update.php
@@ -0,0 +1,14 @@
+<?php
+
+    include 'include/include.php';
+    use DynDns\Datamodel\Domain;
+
+    $domainId = $_GET['domain'];
+    $key1 = $_GET['key1'];
+    $hash2 = $_GET['hash2'];
+    $remoteIp = $_SERVER['REMOTE_ADDR'];
+
+    $domain = new Domain($domainId);
+    $domain->update($key1, $hash2, $remoteIp);
+
+?>
