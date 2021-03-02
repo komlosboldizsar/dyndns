ecf20fcf10fb37172b38e3c3667e3cb69b23c4bb
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-03-02 18:46:43 +0100

    Rename update.php to update-ip.php

--- client/dyndns-update.sh
+++ client/dyndns-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # >> Configuration start
-URL="https://lipotigyor.hu/dyndns/update.php"
+URL="https://lipotigyor.hu/dyndns/update-ip.php"
 DOMAIN_ID=1
 KEY1="7a7e8a78313b2883977b9c9565f12023"
 KEY2="04f82c44cb5c7182d6475fbee9d1d81f"
