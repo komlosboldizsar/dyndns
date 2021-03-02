618f64a4b48cba384adc2696e9b52137c012b058
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2021-03-02 20:05:37 +0100

    Fix client/dyndns-update.sh

--- client/dyndns-update.sh
+++ client/dyndns-update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-source ./dyndns-update-config.sh
+# Find container directory of this script (@source https://stackoverflow.com/a/246128)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+MYDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+# Import configuration
+source "${MYDIR}/dyndns-update-config.sh"
+# Do the update
 DATETIME=`date +"%Y%m%d%H%M%S"`
 HASH2PLAIN="${KEY2}:${DATETIME}"
 HASH2=`echo -n $HASH2PLAIN | md5sum | cut -c1-32`
