884db525026150e25f08eb3d7d18b93cf94420b1
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2021-03-02 20:53:24 +0100

    Fix update-zone-files-and-reload-bind.sh to find absolute path of PHP script

--- cli/update-zone-files-and-reload-bind.sh
+++ cli/update-zone-files-and-reload-bind.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-UPDATE_OUTPUT=`php ./update-zone-files.php`
+# Find container directory of this script (@source https://stackoverflow.com/a/246128)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+MYDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+# Do the update
+UPDATE_OUTPUT=`php ${MYDIR}/update-zone-files.php`
 UPDATED_FILES=`echo $UPDATE_OUTPUT | wc -l`
 if [ "$UPDATED_FILES" -gt 0 ]; then
   /etc/init.d/bind9 reload
