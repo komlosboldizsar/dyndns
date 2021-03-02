aa0c6ec2344720c96039b1507681c208227bc026
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-03-02 18:49:42 +0100

    Separate config from updater script in client/

---
+++ client/dyndns-update-config-example.sh
@@ -0,0 +1,4 @@
+URL="https://example.com/dyndns/update-ip.php"
+DOMAIN_ID=0
+KEY1="<32 character long KEY-1>"
+KEY2="<32 character long KEY-2>"
--- client/dyndns-update.sh
+++ client/dyndns-update.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# >> Configuration start
-URL="https://lipotigyor.hu/dyndns/update-ip.php"
-DOMAIN_ID=1
-KEY1="7a7e8a78313b2883977b9c9565f12023"
-KEY2="04f82c44cb5c7182d6475fbee9d1d81f"
-# << Configuration end
-
-# Don't change code below
+source ./dyndns-update-config.sh
 DATETIME=`date +"%Y%m%d%H%M%S"`
 HASH2PLAIN="${KEY2}:${DATETIME}"
 HASH2=`echo -n $HASH2PLAIN | md5sum | cut -c1-32`
